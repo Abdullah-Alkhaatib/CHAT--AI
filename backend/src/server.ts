aa4f6a54511f56bbaeeb77ffa17efe5aed01d8b7
Abdullah <159600028+Abdullah-Alkhaatib@users.noreply.github.com>
2026-09-21 11:38:11 +0300

    deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,20 +28,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-            return;
-        }
-
-        callback(new Error('Not allowed by CORS'));
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 app.use(express.json());
 
 connectToDB();
